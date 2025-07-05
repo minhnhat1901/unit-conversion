8916dad2ee62e7421b287f0cbf77acc02c29da08
Minh Duong <64678096+minhnhat1901@users.noreply.github.com>
2025-07-04 23:53:10 -0700

    Refine the script to not spam convert button bug

--- index.js
+++ index.js
@@ -12,23 +12,17 @@ const massEl = document.getElementById("mass");
 
 convertBtn.addEventListener("click", function() {
     let val = inputEl.value;
-    lengthEl.innerHTML += `
-        <p>
-            ${val} meters = ${meterToFeet(val)} feet | 
-            ${val} feet = ${feetToMeter(val)} meters
-        </p>
+    lengthEl.textContent = `
+        ${val} meters = ${meterToFeet(val)} feet | 
+        ${val} feet = ${feetToMeter(val)} meters
     `;
-    volumeEl.innerHTML += `
-        <p>
-            ${val} liters = ${literToGallon(val)} gallons | 
-            ${val} gallons = ${gallonToLiter(val)} liters
-        </p>
+    volumeEl.textContent = `
+        ${val} liters = ${literToGallon(val)} gallons | 
+        ${val} gallons = ${gallonToLiter(val)} liters
     `;
-    massEl.innerHTML += `
-        <p>
-            ${val} kilos = ${kilogramToPound(val)} pounds | 
-            ${val} pounds = ${poundToKilogram(val)} kilos
-        </p>
+    massEl.textContent = `
+        ${val} kilos = ${kilogramToPound(val)} pounds | 
+        ${val} pounds = ${poundToKilogram(val)} kilos
     `;
 });
 
